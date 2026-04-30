1492c8ad8ba953d6c4939f66d17fab77ee9258cd
grch <grch@DESKTOP-38G2HV1>
2026-04-30 22:36:22 +0800

    fix

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
+#include <locale.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
-#include <locale.h>
 
 // You need a little-endian machine to run this
 
-#define MZ_MAGIC_NUMBER ((int16_t)0x5a4d)     // "MZ"
-#define PE_MAGIC_NUMBER ((int32_t)0x00004550) // "PE\0\0"
+#define MZ_MAGIC_NUMBER ((int16_t)0x5a4d)      // "MZ"
+#define PE_MAGIC_NUMBER ((int32_t)0x00004550)  // "PE\0\0"
 
 #define MACHINE_TYPE_I386 0x014c
 
@@ -24,18 +24,18 @@ long FileSize(FILE* fp) {
   return size;
 }
 
-uint32_t GetPEHeaderOffset(const char* image, size_t size) {
+// Returns 0 if the file is not a valid PE file
+// uint32_t is enough because the maximum size of a PE file is 4GB
+uint32_t GetPEHeaderOffset(const char* image, uint32_t size) {
   if (size < 0x3c) return 0;
 
-  if (*(int16_t*)image != MZ_MAGIC_NUMBER)
-    return 0;
+  if (*(int16_t*)image != MZ_MAGIC_NUMBER) return 0;
 
   uint32_t offset = *(uint32_t*)(image + 0x3c);
 
   if (offset > size) return 0;
 
-  if (*(int32_t*)(image + offset) != PE_MAGIC_NUMBER)
-    return 0;
+  if (*(int32_t*)(image + offset) != PE_MAGIC_NUMBER) return 0;
 
   return offset;
 }
@@ -50,52 +50,68 @@ uint16_t* GetCharacteristics(const char* image, uint32_t offset) {
 
 int main(void) {
   setlocale(LC_ALL, ".UTF-8");
+
   printf("Input the path to the executable file you want to patch: ");
   char path[PATH_LENGTH];
   fgets(path, sizeof(path), stdin);
   char* newline = strchr(path, '\n');
-  if (newline != NULL)
-    *newline = '\0';
+  if (newline != NULL) *newline = '\0';  // discard newline
+
   FILE* fp = fopen(path, "rb");
   if (fp == NULL) {
     perror("Error opening file");
-    fputs("Do not include quotes around the path", stderr);
+    fputs("Do not include quotes around the path\n", stderr);
     return 1;
   }
-  size_t size = FileSize(fp);
+  long size = FileSize(fp);
   char* buf = malloc(size);
+  if (buf == NULL) {
+    fputs("Error allocating memory, maybe the file is too large\n", stderr);
+    fclose(fp);
+    return 1;
+  }
   fread(buf, size, 1, fp);
   fclose(fp);
+
   uint32_t offset = GetPEHeaderOffset(buf, size);
   if (offset == 0) {
-    fputs("File is not a valid executable", stderr);
+    fputs("File is not a valid executable\n", stderr);
     free(buf);
     return 1;
   }
+
   uint16_t machineType = GetMachineType(buf, offset);
   if (machineType != MACHINE_TYPE_I386) {
-    fputs("File is not a 32-bit executable", stderr);
+    fputs(
+        "File is not a 32-bit executable\n"
+        "64-bit executables do not need to be patched\n",
+        stderr);
     free(buf);
     return 1;
   }
+
   uint16_t* characteristics = GetCharacteristics(buf, offset);
   if (*characteristics & LARGE_ADDRESS_AWARE) {
-    fputs("File is already large address aware", stderr);
+    fputs("File is already large address aware\n", stderr);
     free(buf);
     return 1;
   }
+
+  // Backing up the original file with a .bak extension
   char backupPath[PATH_LENGTH];
   strcpy(backupPath, path);
-  strcat(backupPath, ".bak");
+  strncat(backupPath, ".bak", PATH_LENGTH - strlen(path) - 1);
   int result = rename(path, backupPath);
   if (result != 0) {
-    printf("Error backing up original file, do you wish to overwrite it? (y/n)");
+    printf(
+        "Error backing up original file, do you wish to overwrite it? (y/n)");
     int ch = getchar();
     if (ch != 'y') {
       free(buf);
       return 1;
     }
   }
+
   *characteristics |= LARGE_ADDRESS_AWARE;
   FILE* newFp = fopen(path, "wb");
   if (newFp == NULL) {
